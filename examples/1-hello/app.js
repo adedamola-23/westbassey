64e5b5bd8ed0ad4a114c2c69fde5cfdba1913c53
adedamola-23 <ahighprogrammer@gmail.com>
2021-05-11 07:20:24 -0700

    cors error fixed

--- examples/1-hello/app.js
+++ examples/1-hello/app.js
@@ -4,7 +4,7 @@ const result =  document.querySelector('.result')
 
 const fetchData = async () => {
  try {
-     const {data} = await axios.get('/api/1-hello')
+     const {data} = await axios.get('ogabassey.netlify.app/api/2-basic-api')
      result.textContent = data
  } catch (error) {
      console.log(error.response);
